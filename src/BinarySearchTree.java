8d8b581be8c67d2bdf5c5563b334b45c859c91d9
jmyanez <jmyanez@miners.utep.edu>
2018-11-07 18:45:22 -0700

    Initial commit

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,70 @@
+public class BinarySearchTree {
+    class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    // Root of BST
+    Node root;
+
+    // Constructor
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    /* A recursive function to insert a new key in BST */
+    Node insertRec(Node root, int key) {
+
+        /* If the tree is empty, return a new node */
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        /* Otherwise, recur down the tree */
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        /* return the (unchanged) node pointer */
+        return root;
+    }
+
+    void inorder()  {
+        inorderRec(root);
+    }
+
+    // A utility function to do inorder traversal of BST
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
+
+    int maxDepth(Node root){
+        if(root == null){
+            return 1;
+        }
+        else {
+            int maxLeft = maxDepth(root.left);
+            int maxRight = maxDepth(root.right);
+            if(maxLeft > maxRight){
+                return maxLeft+1;
+            }
+            else
+                return maxRight+1;
+        }
+    }
+}
